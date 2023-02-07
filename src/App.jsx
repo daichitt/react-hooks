ec176c91bd20c2b9c5738f93ed751dc119bc6932
daichitt <gene.sixteen.000925@outlook.jp>
2023-02-07 17:23:09 +0900

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { React, useEffect, useState, useContext, useRef } from 'react'
+import { React, useEffect, useState, useContext, useRef, useReducer } from 'react'
 import CodeContext from './main';
 
 
+const reducer = (state, action) => {
+  console.log("actionは" , action, "です")
+  console.log("stateは" , state, "です")
+  switch (action.type) {
+    case 'increment': {
+      return state + 1;
+    }
+    case 'decrement': {
+      return state - 1;
+    }
+    default: state;
+  }
+};
 function App() {
   const [count, setCounter] = useState(0);
   const codeinfo = useContext(CodeContext);
@@ -12,6 +25,9 @@ function App() {
   // 文字列を取得する際などに使用する
   const ref = useRef();
 
+  // (state, action) => newState という型のリデューサ (reducer) を受け取り、現在の state を dispatch メソッドとペアにして返します
+  // https://beta.reactjs.org/reference/react/useReducer#usage
+  const [state, dispatch] = useReducer(reducer, 0);
   const increase = () => {
     if(count == 10) return;
     setCounter(count + 1);
@@ -41,10 +57,16 @@ function App() {
       <h1>useContext</h1>
       <p>{codeinfo.name}</p>
       <p>{codeinfo.age}</p>
-
+      <hr />
       <h1>useRef</h1>
       <input class="yamada" id="refrefref" ref={ref} type="text" />
       <button onClick={handleRef}>UseRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>Count : {state}</p>
+      <button onClick={() => { dispatch({ type: 'increment' })}}> + </button>
+      <button onClick={() => { dispatch({ type: 'decrement' })}}> - </button>
     </div>
   )
 }
